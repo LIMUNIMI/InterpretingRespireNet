0232b513b7694d59a493bd1c725e229322a05a66
iamgiokali <118732315+iamgiokali@users.noreply.github.com>
2024-10-29 12:45:55 +0100

    Create posthoc_script.sh

---
+++ scripts/posthoc_script.sh
@@ -0,0 +1,1 @@
+python posthoc.py --data_dir ./data/icbhi_dataset/audio_text_data/ --checkpoint models/ckpt_best.pkl --folds_file ./data/patient_list_foldwise.txt --output_dir ./xai_results/ --sample_index 0
